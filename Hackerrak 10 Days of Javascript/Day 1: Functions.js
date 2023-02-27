3aeb5480bb394ab6adcdde5da6b9ef0e00a8e92f
Siddharthan P S <67634528+sidshas03@users.noreply.github.com>
2023-02-27 14:09:18 +0530

    Create Day 1: Functions.js

---
+++ Hackerrak 10 Days of Javascript/Day 1: Functions.js
@@ -0,0 +1,30 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function factorial(n) {
+    let result = n;
+    for (var i = 1; i < n; i++) {
+        result = result * (n - i);
+    }
+    return result;
+}
